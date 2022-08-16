4a622aba563520bee8f2b9f37f06d31a569c4deb
Elías <eliascardonarod@gmail.com>
2022-08-16 12:10:55 -0500

    Commit #4. Corriguiendo el prolema con los estilos

--- components/sections/SectionEleven.js
+++ components/sections/SectionEleven.js
@@ -5,20 +5,20 @@ import styles from '../../styles/Landing.module.css'
 export const SectionEleven = () => {
   return (
     <>
-        <div className={styles.ctaSection}>
-        <div className={styles.centeredGrid}>
-          <div className={styles.textCenter}>
-            <h1>Let's do it!</h1>
-            <Link href="/payment">
-              <button type="button" className={styles.sellBtn}>
-                <ion-icon name="bag-handle-outline" />
-                <span>Get into!</span>
-                <small>lorem ipsum dolor sit amet consectetur</small>
-              </button>
-            </Link>
-          </div>
+    <div className={styles.ctaSection}>
+      <div className={styles.centeredGrid}>
+        <div className={styles.textCenter}>
+          <h1>Let's do it!</h1>
+          <Link href="/payment">
+            <button type="button" className={styles.sellBtn}>
+              <ion-icon name="bag-handle-outline" />
+              <span>Get into!</span>
+              <small>lorem ipsum dolor sit amet consectetur</small>
+            </button>
+          </Link>
         </div>
-        </div>
+      </div>
+    </div>
     </>
   )
 }
